7dd03f5ed62c72021842e56cd19a1280903f03a3
Suzibur <suzibur@.com>
2020-10-18 18:58:34 +0600

    Environment value submitted on heroku

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('service'))
 app.use(fileUpload());
-const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.wanio.mongodb.net/CreativeAgency?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wanio.mongodb.net/CreativeAgency?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
